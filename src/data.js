7b400fb4b79cbb9ab12a7f1b2ee5c71b8e604ee7
sdharani28 <96778054+sdharani28@users.noreply.github.com>
2022-06-19 21:14:44 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,27 @@
+let invoices = [
+    {
+      name: "Santa Monica",
+      number: 1995,
+      amount: "$10,800",
+      due: "12/05/1995",
+    },
+    {
+      name: "Stankonia",
+      number: 2000,
+      amount: "$8,000",
+      due: "10/31/2000",
+    },
+    {
+      name: "Wide Open Spaces",
+      number: 1998,
+      amount: "$4,600",
+      due: "01/27/1998",
+    },
+  ];
+  
+  export function getInvoices() {
+    return invoices;
+  }
+
+
+  
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import App from "./App";
+import Expenses from "./expenses";
+import Invoices from "./invoices";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} >
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+        <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
+
